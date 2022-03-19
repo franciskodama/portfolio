3eba9658e290d4b9b9c11feee3560193c7d08e50
Kodama <fk@fkodama.com>
2022-03-19 19:46:27 -0400

    New layout for projects area

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,21 +4,29 @@ import CardIcon from '../assets/images/card-icon.svg'
 
 function Card({ project }) {
 
+ const handleClick = e => {
+   e.target.closest('.card-container').classList.add('card-container--active')
+  console.log(e.target)
+ }
+
   return (
-    <div className='card-container'>
-      <img className='card-image' src={project.image} alt='main image project'/>
+
+    <div onClick={handleClick} className='card-container'>
       <p className='card-category'>{project.category}</p>
-      
-      <div className="title-wraper">
-        <h3 className='card-title'>{project.title}</h3>
-        <h3 className='card-tech'>{project.tech}</h3>
+      <img className='card-image' src={project.image} alt='main image project'/>
+      <div className="card-box">
+        <img className='card-icon' src={project.icon} alt='icon project'/>
+        <div className="title-wrapper">
+          <h3 className='card-title'>{project.title}</h3>
+          <h3 className='card-tech'>{project.tech}</h3>
+        </div>
       </div>
-
+      <p className='card-description'>{project.description}</p>
+      <p className='card-text'>{project.text}</p>
       <a className="explore-wraper" href={project.url} target="_blank">
-        <p className='card-explore' >explore project</p>
-        <img className='card-icon' src={CardIcon} alt='icon explore project'/>
+        <p className='card-explore' >visit project</p>
+        <img className='card-icon-visit' src={CardIcon} alt='icon explore project'/>
       </a>
-
     </div>
   )
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -8,43 +8,58 @@
     [
       { 
         id: 1,
-        title: 'this.portfolio',
+        icon: require('../assets/images/card-icon-thisportfolio.png'),
+        title: 'this. portfolio',
         tech: '... + react',
         category: 'website',
         url: 'https://www.fkodama.com',
-        image: require('../assets/images/card-squared-this.jpg')
+        image: require('../assets/images/card-squared-this.jpg'),
+        description: 'I am an experienced tech services entrepreneur/consultant product.',
+        text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five but also the leap into electronic remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
       },
       {    
         id: 2,
+        icon: require('../assets/images/card-icon-pacman.png'),
         title: 'pac man',
         tech: 'html css js',
         category: 'video game',
         url: 'https://fkodama.com/pacman/',
-        image: require('../assets/images/card-squared-pacman2.jpg')
+        image: require('../assets/images/card-squared-pacman2.jpg'),
+        description: 'I am an experienced tech services entrepreneur/consultant product.',
+        text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five but also the leap into electronic remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
       },
       { 
         id: 3,
+        icon: require('../assets/images/card-icon-spaceinvaders.png'),
         title: 'space invaders',
         tech: 'html css js',
         category: 'video game',
         url: 'https://fkodama.com/spaceinvaders/',
         image: require('../assets/images/card-squared-space.jpg'),
+        description: 'I am an experienced tech services entrepreneur/consultant product.',
+        text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five but also the leap into electronic remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
       },
       { 
         id: 4,
+        icon: require('../assets/images/card-icon-seletos.png'),
         title: 'seletos',
         tech: 'html css js',
         category: 'website',
         url: 'https://fkodama.com/_dev/seletos',
-        image: require('../assets/images/card-squared-seletos.jpg')
+        image: require('../assets/images/card-squared-seletos.jpg'),
+        description: 'I am an experienced tech services entrepreneur/consultant product.',
+        text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five but also the leap into electronic remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
       },
       { 
         id: 5,
+        icon: require('../assets/images/card-icon-mundial.png'),
         title: 'mundial',
         tech: 'html css',
         category: 'website',
         url: 'https://www.mundialcomunicacao.com.br/',
-        image: require('../assets/images/card-squared-mundial.jpg')
+        image: require('../assets/images/card-squared-mundial.jpg'),
+        description: 'I am an experienced tech services entrepreneur/consultant product.',
+        text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five but also the leap into electronic remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
       }    
     ] 
   )
