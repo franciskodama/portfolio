f1bdaa39cc9324f3810bdef5a68d9c8a9ac049ca
Kodama <fk@fkodama.com>
2022-03-21 21:32:07 -0400

    Fix fonts of data in Api area

--- src/pages/Api.js
+++ src/pages/Api.js
@@ -29,7 +29,8 @@ function Api() {
             <div className="api--grid">
 
               <div className="question-wrapper">              
-                <h2 className="api--question">What city are you in?</h2>
+                <h2 className="api--question">What city</h2>
+                <h2 className="api--question">are you in?</h2>
                 <input value={location} onChange={event => setLocation(event.target.value)} onKeyPress={searchLocation} placeholder='enter location' type='text' />
               </div>
 
@@ -44,53 +45,53 @@ function Api() {
 
               <div className="feels-wrapper">
                 <h4 className="api--title-others">Feels like</h4>
-                <h3>{data.main ? <p>{data.main.feels_like}</p> : null}</h3>
+                <h3>{data.main ? <p className='data'>{Math.trunc(data.main.feels_like)}</p> : null}</h3>
                 <h5 className="api--unit">Celsius</h5>
               </div>
 
               <div className="temp-wrapper">
                 <h4 className="api--title-temp">Current weather</h4>
-                <h3>{data.main ? <p>{data.main.temp}</p> : null}</h3>
+                <h3>{data.main ? <p className='temp'>{Math.trunc(data.main.temp)}°C</p> : null}</h3>
               </div>
 
               <div className="wind-wrapper">
                 <h4 className="api--title-others">Wind</h4>
-                <h3>{data.wind ? <p>{data.wind.speed}</p> : null}</h3>
+                <h3>{data.wind ? <p className='data'>{data.wind.speed}</p> : null}</h3>
                 <h5 className="api--unit">km/h</h5>
               </div>
 
               <div className="pressure-wrapper">
                 <h4 className="api--title-others">Pressure</h4>
-                <h3>{data.main ? <p>{data.main.pressure}</p> : null}</h3>
+                <h3>{data.main ? <p className='data'>{data.main.pressure}</p> : null}</h3>
                 <h5 className="api--unit">kPa</h5>
               </div>
 
               <div className="humidity-wrapper">
                 <h4 className="api--title-others">Humidity</h4>
-                <h3>{data.main ? <p>{data.main.humidity}</p> : null}</h3>
+                <h3>{data.main ? <p className='data'>{data.main.humidity}</p> : null}</h3>
                 <h5 className="api--unit">%</h5>
               </div>
 
               <div className="visibility-wrapper">
                 <h4 className="api--title-others">Visibility</h4>
-                <h3>{data.visibility}</h3>
+                <p className='data'>{data.visibility}</p>
                 <h5 className="api--unit">km</h5>
               </div>
 
               <div className="sunrise-wrapper">
                 <h4 className="api--title-others">Sunrise</h4>
-                <h3>{data.sys ? <p>{data.sys.sunrise}</p> : null}</h3>
+                <h3>{data.sys ? <p className='data'>{data.sys.sunrise}</p> : null}</h3>
                 <h5 className="api--unit">a.m.</h5>
               </div>
 
               <div className="sunset-wrapper">
                 <h4 className="api--title-others">Sunset</h4>
-                <h3>{data.sys ? <p>{data.sys.sunset}</p> : null}</h3>
+                <h3>{data.sys ? <p className='data'>{data.sys.sunset}</p> : null}</h3>
                 <h5 className="api--unit">p.m.</h5>
               </div>
 
               <div className="updated-wrapper">
-                <h4 className="api--title-others">Updated on</h4>
+                <h4 className="api--date">Updated on</h4>
                 <h3 className='api--date'>Sunday, 1:00 p.m.</h3>
               </div>
 
