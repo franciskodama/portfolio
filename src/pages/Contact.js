af76d6bbe50a0029d919f8ba0717f860e999233c
Kodama <fk@fkodama.com>
2022-03-30 11:46:57 -0400

    Logic until send message for contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
-import { v4 as uuidv4 } from 'uuid';
 import '../styles/Contact.css'
 import Button from '../components/Button'
 
@@ -35,7 +34,7 @@ const data = [
   },
   {
     id: '8',
-    content: 'I did not like your website.'
+    content: 'I am looking for a professional like you.'
   },
   {
     id: '9',
@@ -60,11 +59,11 @@ const data = [
 ]
 
 const dropSpace = {
-  [uuidv4()]: {
+  drag: {
     name: 'drag from here:',
     items: data
   },
-  [uuidv4()]: {
+  drop: {
     name: 'drop here:',
     items: []
   }
@@ -92,7 +91,6 @@ const onDragEnd = (result, columns, setColumns) => {
         items: destItems
       }
     })
-    console.log(columns)
   } else {
     const column = columns[source.droppableId]
     const copiedItems = [...column.items]
@@ -114,13 +112,18 @@ const Contact = () => {
   const [columns, setColumns] = useState(dropSpace);
   const [ message, setMessage ] = useState('')
 
+
+
+  const handleChange = (event) => {
+    setMessage(event.target.value)
+  }
+
   const handleClickClearMessage = () => {
     setMessage('')
   }
 
-  const handleChange = (event) => {
-    setMessage(event.target.value)
-    console.log(message)
+  const handleClickSendMessage = () => {
+    console.log(columns.drop.items.map((element) => element.content) + message)
   }
 
   return (
@@ -184,258 +187,31 @@ const Contact = () => {
       </DragDropContext>
 
     <form className='form-contact'>
-            <p className='form-contact__title'>Additional comments:</p>
-            <textarea
-              className='form-contact__input'
-              placeholder='type some additional comments and sign your name.'
-              value={message}
-              onChange={handleChange}
-              type='text'
-            ></textarea>
+      <p className='form-contact__title'>Additional comments:</p>
+      <textarea
+        className='form-contact__input'
+        placeholder='type some additional comments and sign your name.'
+        value={message}
+        onChange={handleChange}
+        type='text'
+      ></textarea>
 
-            <div className='form-contact__buttons'>
-              <Button
-                className={'btn btn--dark-dark-bg'}
-                onClick={handleClickClearMessage}
-                text='clear'
-                align='flex-start'
-              />
-              <Button
-                text='send'
-                align='flex-end'
-              />
-            </div>
-          </form>
-
-
-</div>
+      <div className='form-contact__buttons'>
+        <Button
+          className={'btn btn--dark-dark-bg'}
+          onClick={handleClickClearMessage}
+          text='clear'
+          align='flex-start'
+        />
+        <Button
+        onClick={handleClickSendMessage}
+          text='send'
+          align='flex-end'
+        />
+      </div>
+    </form>
+  </div>
 </section>
 )}
 
 export default Contact
-
-  // const [ list, setList ] = useState(data)
-
-  // const reorderList = (list, startIndex, endIndex) => {
-  //   const result = Array.from(list)
-  //   const [removed] = result.splice(startIndex, 1)
-  //   result.splice(endIndex, 0, removed)
-  //   return result
-  // }
-
-  // const onEnd = (result) => {
-  //   setList(reorderList(list, result.source.index, result.destination.index))
-  // }
-
-//   return (
-//     <section className='section section--contact' id='contact'>
-
-//         <div className='container'>
-//           <h1 className='main-title'>hello generator</h1>
-//           <p className='sub-title'>Let me help you drop me a line! ;)</p>
-
-//           <DragDropContext onDragEnd={onEnd}>  
-//             <Droppable droppableId='dragdrop'>
-//               {(provided, snapshot) => ( <div ref={provided.innerRef}>
-//                 {list.map((item, index) => (
-//                   <Draggable draggableId={item.id} key={item.id} index={index} >
-//                     {(provided, snapshot) => (
-//                       <div
-//                       ref={provided.innerRef}
-//                       {...provided.draggableProps}
-//                       {...provided.dragHandleProps}
-//                       >
-//                         <div className='phrase'>
-//                           {item.text}
-//                         </div>
-//                       </div>
-//                      )}
-//                   </Draggable>                  
-//                 ))}
-//                 {provided.placeholder}
-//                 </div>
-//               )}
-//             </Droppable>
-//           </DragDropContext>
-//           </div>
-//     </section>
-//   )
-// }
-
-// export default Contact
-
-  // const [ message, setMessage ] = useState('')
-
-  // const handleClickClearMessage = () => {
-  //   setMessage('')
-  // }
-
-{/* 
-          <div className='drop'>
-            <p className='drop__title'>drop here:</p>
-            <div className='drop__space'>
-            </div>
-          </div> */}
-
-
-          {/* <form className='form-contact'>
-            <p className='form-contact__title'>Additional comments:</p>
-            <textarea
-              value=''
-              className='form-contact__input'
-              type='text'
-              size='10'
-            ></textarea>
-
-            <div className='form-contact__buttons'>
-              <Button
-                className={'btn btn--dark-dark-bg'}
-                onClick={handleClickClearMessage}
-                text='clear'
-                align='flex-start'
-              />
-              <Button
-                text='send'
-                align='flex-end'
-              />
-            </div>
-          </form> */}
-
-
-
-          {/* <ul className='phrase-container'>
-            <li className='phrase'>I hope this message finds you well.</li>
-            <li className='phrase'>Cool website, man!</li>
-            <li className='phrase highlight'>do you want to work in our company?</li>
-            <li className='phrase'>ops, I found a bug.</li>
-            <li className='phrase'>Holly cow, what a portfolio!</li>
-            <li className='phrase highlight'>Let's schedule a interview?</li>
-            <li className='phrase'>Cool!!!</li>
-            <li className='phrase'>I didn't like your website, and I tell you why...</li>
-            <li className='phrase'>regards,</li>
-            <li className='phrase'>sincerely,</li>
-            <li className='phrase'>Take care!</li>
-            <li className='phrase'>Thanks! :)</li>
-          </ul> */}
-
-
-
-
-//           import React, { useState } from 'react'
-// import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
-// import Button from '../components/Button'
-// import '../styles/Contact.css'
-
-// const Contact = () => {
-
-//   const [ message, setMessage ] = useState('')
-
-//   const handleClick = (event) => {
-//     event.preventDefault()
-//     setMessage(previousMessage => previousMessage + event.target.value)
-//   }
-
-//   const handleChange = (event) => {
-//     setMessage(previousMessage => previousMessage + event.target.value)
-//     console.log(message)
-//   }
-
-//   const handleClickClearMessage = () => {
-//     setMessage('')
-//   }
-
-//   return (
-//     <section className='section section-contact' id='contact'>
-
-//         <div className='container'>
-//           <h1 className='title'>hello generator</h1>
-//           <p className='sub-title'>Let me help you drop me a line! ;)</p>
-
-//           <DragDropContext>  
-//             <form>
-//             <p className='sub-title-tray sub-title-tray-first'>drag from here:</p>
-//               <input onClick={handleClick} className='phrase phrase--active' value='Hey Francis!' type='submit'/>
-//               <label htmlFor='Hey Francis!'></label>
-
-//               <input onClick={handleClick} className='phrase' value='I hope this message finds you well.' type='submit'/>
-//               <label htmlFor='I hope this message finds you well.'></label>
-
-//               <input onClick={handleClick} className='phrase' value='Cool website, man!' type='submit'/>
-//               <label htmlFor='Cool website, man!'></label>
-
-//               <input onClick={handleClick} className='phrase' value='do you want to work in our company?' type='submit'/>
-//               <label htmlFor='do you want to work in our company?'></label>
-
-//               <input onClick={handleClick} className='phrase' value='ops, I found a bug.' type='submit'/>
-//               <label htmlFor='ops, I found a bug.'></label>
-
-//               <input onClick={handleClick} className='phrase' value='Holly cow, what a portfolio!' type='submit'/>
-//               <label htmlFor='Holly cow, what a portfolio!'></label>
-
-//               <input onClick={handleClick} className='phrase' value={`Let's schedule an interview?`} type='submit'/>
-//               <label htmlFor='Lets schedule a interview?'></label>
-
-//               <input onClick={handleClick} className='phrase' value='I did not like your website.' type='submit'/>
-//               <label htmlFor='I did not like your website.'></label>
-
-//               <input onClick={handleClick} className='phrase' value='Cool!!!' type='submit'/>
-//               <label htmlFor='Cool!!!'></label>
-
-//               <input onClick={handleClick} className='phrase' value='I did not like your website, and I tell you why...' type='submit'/>
-//               <label htmlFor='I did not like your website, and I tell you why...'></label>
-
-//               <input onClick={handleClick} className='phrase' value='Regards,' type='submit'/>
-//               <label htmlFor='Regards,'></label>
-
-//               <input onClick={handleClick} className='phrase' value='Take care!' type='submit'/>
-//               <label htmlFor='Take care!'></label>
-
-//               <input onClick={handleClick} className='phrase' value='Thanks! :)' type='submit'/>
-//               <label htmlFor='Thanks! :)'></label>
-//             </form>
-//           </DragDropContext>
-//           <div className='tray-container'>
-//             <p className='sub-title-tray'>drop here:</p>
-//             <div className='tray'>
-
-//             </div>
-//           </div>
-         
-
-//           <form className='form-contact'>
-//           <p className='sub-title-tray'>Additional comments:</p>
-//             <textarea
-//               value={message}
-//               onChange={handleChange}
-//               className='form-contact-input'
-//               type='text'
-//               size='10'
-//             ></textarea>
-
-//             <div className='contact-buttons'>
-
-//             <Button
-//               onClick={handleClickClearMessage}
-//               text='clear'
-//               bgColor='var(--dark-color)'
-//               color='var(--bright-color)'
-//               align='flex-start'
-//               border='1px solid var(--bright-color)'
-//             />
-
-//             <Button
-//               text='send'
-//               bgColor='var(--third-color)'
-//               color='var(--brigth-color'
-//               align='flex-end'
-//             />
-            
-//             </div>
-//           </form>
-//         </div>
-
-//     </section>
-//   )
-// }
-
-// export default Contact
