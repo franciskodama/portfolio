f5b376d3e180cff73fa88633cb68aaf2b048635f
Kodama <fk@fkodama.com>
2022-04-13 16:07:37 -0400

    Footer mobile version

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,66 +1,161 @@
-import React from 'react'
-import { Link } from 'react-scroll'
-import LogoFooter from '../assets/images/logo-fkodama-footer.svg'
-import Linkedin from '../assets/images/ico-linkedin-white.svg'
-import Insta from '../assets/images/ico-instagram-white.svg'
-import Face from '../assets/images/ico-facebook-white.svg'
-import Git from '../assets/images/ico-github-white.svg'
+import React from "react";
+import { Link } from "react-scroll";
+import LogoFooter from "../assets/images/logo-fkodama-footer.svg";
+import Linkedin from "../assets/images/ico-linkedin-white.svg";
+import Insta from "../assets/images/ico-instagram-white.svg";
+import Git from "../assets/images/ico-github-white.svg";
 
-import '../styles/Footer.css'
+import "../styles/Footer.css";
 
 const Footer = () => {
-  
   return (
-    <section className='section section-footer'>
-        <div className='container'>
-          <h2 className='footer-title'>ABOUT<span>ME</span></h2>
-          <h2 className='footer-title'>GET<span>BACK</span></h2>
-          <h2 className='footer-title'>GET<span>IN TOUCH</span></h2>
-          <h2 className='footer-title'>GET<span>CONNECTED</span></h2>
+    <section className="section section--footer">
+      <div className="footer__grid">
+        <h2 className="footer__title footer__title-about">
+          ABOUT<span>ME</span>
+        </h2>
+        <h2 className="footer__title footer__title-menu">
+          GET<span>BACK</span>
+        </h2>
+        <h2 className="footer__title footer__title-contact">
+          GET<span>IN TOUCH</span>
+        </h2>
+        <h2 className="footer__title footer__title-icons">
+          GET<span>CONNECTED</span>
+        </h2>
 
-          <p className='aboutme'>A Webdeveloper with a huge experience in marketing, a natural leader, curious, who find engagement challenge himself constantly.</p>
+        <p className="footer__aboutme">
+          A Webdeveloper with a huge experience in marketing, a natural leader,
+          curious, who find engagement challenge himself constantly.
+        </p>
 
-          <div className='menu'>
-            <Link className='menu-item' to='hero' spy={true} smooth={true} offset={50} duration={500}> home </Link>
-            <Link className='menu-item' to='about' spy={true} smooth={true} offset={-150} duration={500}> about </Link>
-            <Link className='menu-item' to='projects' spy={true} smooth={true} offset={-100} duration={500}> projects </Link>
-            <Link className='menu-item' to='api' spy={true} smooth={true} offset={-150} duration={500}> api </Link>
-            <Link className='menu-item' to='code' spy={true} smooth={true} offset={0} duration={500}> clean code </Link>
-            <Link className='menu-item' to='contact' spy={true} smooth={true} offset={-150} duration={500}> contact </Link>
-          </div>
-
-          <div className='contact'>
-            <p className='email'>fk@fkodama.com</p>
-            <Link className='send-message' to='contact' spy={true} smooth={true} offset={-150} duration={500}> send message </Link>
-          </div>
-
-          <div className='icons'>
-            <a className='icon-item' href="https://github.com/franciskodama" target="_blank"><img src={Git} alt='logo Github'/></a>
-            <a className='icon-item' href="https://www.linkedin.com/in/kodama/" target="_blank"><img src={Linkedin} alt='logo Linkedin'/></a>
-            <a className='icon-item' href="https://www.instagram.com/franciskodama/" target="_blank"><img src={Insta} alt='logo Instagram'/></a>
-            {/* <a className='icon-item' href="https://www.facebook.com/francishidekikodama" target="_blank"><img src={Face} alt='logo Facebook'/></a> */}
-          </div>
-
-          <img src={LogoFooter} alt='logo fkodama' />
-
-          <div className='design'>
-            <p>Designed and built by</p>
-            <p className='bold'>Francis Kodama</p>
-            <p>Copyright 2022</p>
-          </div>
+        <div className="footer__menu">
+          <Link
+            className="footer__menu-item"
+            to="hero"
+            spy={true}
+            smooth={true}
+            offset={50}
+            duration={500}
+          >
+            {" "}
+            home{" "}
+          </Link>
+          <Link
+            className="footer__menu-item"
+            to="about"
+            spy={true}
+            smooth={true}
+            offset={-150}
+            duration={500}
+          >
+            {" "}
+            about{" "}
+          </Link>
+          <Link
+            className="footer__menu-item"
+            to="projects"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+          >
+            {" "}
+            projects{" "}
+          </Link>
+          <Link
+            className="footer__menu-item"
+            to="api"
+            spy={true}
+            smooth={true}
+            offset={-150}
+            duration={500}
+          >
+            {" "}
+            api{" "}
+          </Link>
+          <Link
+            className="footer__menu-item"
+            to="code"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+          >
+            {" "}
+            clean code{" "}
+          </Link>
+          <Link
+            className="footer__menu-item"
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={-150}
+            duration={500}
+          >
+            {" "}
+            contact{" "}
+          </Link>
         </div>
 
-        <div className='last-container'>
-            <div className='resume-container'>
-              <h2 className='resume-title'>RESUME</h2>
-              <p>Download</p>
-              <p className='bold'>Resumé</p>
-            </div>
-            <p className='localization'>Ottawa, ON - <span className='bold'>Canada</span></p>
-          </div>
+        <div className="footer__contact">
+          <p className="footer__email">fk@fkodama.com</p>
+          <Link
+            className="footer__send"
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={-150}
+            duration={500}
+          >
+            {" "}
+            send message{" "}
+          </Link>
+        </div>
 
+        <div className="footer__icons">
+          <a
+            className="footer__icon-item"
+            href="https://github.com/franciskodama"
+            target="_blank"
+          >
+            <img src={Git} alt="logo Github" />
+          </a>
+          <a
+            className="footer__icon-item"
+            href="https://www.linkedin.com/in/kodama/"
+            target="_blank"
+          >
+            <img src={Linkedin} alt="logo Linkedin" />
+          </a>
+          <a
+            className="footer__icon-item"
+            href="https://www.instagram.com/franciskodama/"
+            target="_blank"
+          >
+            <img src={Insta} alt="logo Instagram" />
+          </a>
+        </div>
+
+        <img className="footer__logo" src={LogoFooter} alt="logo fkodama" />
+
+        <div className="footer__design">
+          <p>Designed and built by</p>
+          <p className="footer--bold">Francis Kodama</p>
+          <p>Copyright 2022</p>
+        </div>
+
+        <div className="footer__resume">
+          <h2 className="footer__resume-title">RESUME</h2>
+          <p>Download</p>
+          <p className="footer--bold">Resumé</p>
+        </div>
+        <p className="footer__localization">
+          Ottawa, ON - <span className="footer-bold">Canada</span>
+        </p>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
