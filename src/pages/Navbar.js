2fc59bd2798f678b6d13c77f452813c8100217f9
Kodama <fk@fkodama.com>
2022-04-16 15:17:17 -0400

    Fix navbar for all versions

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Link } from "react-scroll";
 import "../styles/Navbar.css";
 import "../App.css";
@@ -31,6 +31,9 @@ const Navbar = () => {
         <Link to="hero" spy={true} smooth={true} offset={50} duration={500}>
           <img className="navbar__logo" src={Logo} />
         </Link>
+
+        {/* -------- NAVBAR TOGGLE -------- */}
+
         <div className="navbar__toggle">
           <img
             className="navbar__menu"
@@ -52,10 +55,90 @@ const Navbar = () => {
             alt="close icon menu"
           />
         </div>
+
+        {/* -------- NAVBAR EXTENDED -------- */}
+
         <nav
-          className="navbar"
-          style={{ display: isActive ? "block" : "none" }}
+          className="navbar__extended"
+          style={{ display: isOpen ? "block" : "none" }}
         >
+          <Link
+            className="navbar__item"
+            to="hero"
+            spy={true}
+            smooth={true}
+            offset={50}
+            duration={500}
+            onClick={handleClickOnLink}
+          >
+            {" "}
+            home{" "}
+          </Link>
+          <Link
+            className="navbar__item"
+            to="about"
+            spy={true}
+            smooth={true}
+            offset={-150}
+            duration={500}
+            onClick={handleClickOnLink}
+          >
+            {" "}
+            about me{" "}
+          </Link>
+          <Link
+            className="navbar__item"
+            to="projects"
+            spy={true}
+            smooth={true}
+            offset={-100}
+            duration={500}
+            onClick={handleClickOnLink}
+          >
+            {" "}
+            projects{" "}
+          </Link>
+          <Link
+            className="navbar__item"
+            to="api"
+            spy={true}
+            smooth={true}
+            offset={-150}
+            duration={500}
+            onClick={handleClickOnLink}
+          >
+            {" "}
+            api{" "}
+          </Link>
+          <Link
+            className="navbar__item"
+            to="code"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={500}
+            onClick={handleClickOnLink}
+          >
+            {" "}
+            my code{" "}
+          </Link>
+          <Link
+            className="navbar__item"
+            to="contact"
+            spy={true}
+            smooth={true}
+            offset={-150}
+            duration={500}
+            onClick={handleClickOnLink}
+          >
+            {" "}
+            contact{" "}
+          </Link>
+        </nav>
+
+        {/* -------- NAVBAR HORIZONTAL -------- */}
+
+        <nav className="navbar">
           <Link
             className="navbar__item"
             to="hero"
