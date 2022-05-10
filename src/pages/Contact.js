e9cfa46c6aa0f7a64a2337866b8e4888e1cea9da
Kodama <fk@fkodama.com>
2022-05-10 11:51:24 -0400

    Delete effect opacity in contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -111,139 +111,136 @@ const Contact = () => {
   const [columns, setColumns] = useState(dropSpace);
 
   return (
-    <Parallax opacity={[0, 2]}>
-      <section className="section contact" id="contact">
-        <div className="container">
-          <h1 className="section-title" style={{ textAlign: "left" }}>
-            hello generator
-          </h1>
-          <p>Let me help you drop me a line! ;)</p>
-
-          <DragDropContext
-            onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
-          >
-            {Object.entries(columns).map(([columnId, column], index) => {
-              return (
-                <div className="drop" key={columnId}>
-                  <h2 className="drop__title">{column.name}</h2>
-                  <div>
-                    <Droppable droppableId={columnId} key={columnId}>
-                      {(provided, snapshot) => {
-                        return (
-                          <div
-                            {...provided.droppableProps}
-                            ref={provided.innerRef}
-                            className="drop__space"
-                            style={{
-                              background: snapshot.isDraggingOver
-                                ? "rgba(255, 255, 255, 0.1)"
-                                : "var(--dark-color)",
-                            }}
-                          >
-                            {column.items.map((item, index) => {
-                              return (
-                                <Draggable
-                                  key={item.id}
-                                  draggableId={item.id}
-                                  index={index}
-                                >
-                                  {(provided, snapshot) => {
-                                    return (
-                                      <div
-                                        className="phrase"
-                                        ref={provided.innerRef}
-                                        {...provided.draggableProps}
-                                        {...provided.dragHandleProps}
-                                        style={{
-                                          backgroundColor: snapshot.isDragging
-                                            ? "var(--third-color)"
-                                            : "var(--dark-color)",
-                                          color: "var(--bright-color)",
-                                          ...provided.draggableProps.style,
-                                        }}
-                                      >
-                                        {item.content}
-                                      </div>
-                                    );
-                                  }}
-                                </Draggable>
-                              );
-                            })}
-                            {provided.placeholder}
-                          </div>
-                        );
-                      }}
-                    </Droppable>
-                  </div>
+    <section className="section contact" id="contact">
+      <div className="container">
+        <h1 className="section-title" style={{ textAlign: "left" }}>
+          hello generator
+        </h1>
+        <p>Let me help you drop me a line! ;)</p>
+        <DragDropContext
+          onDragEnd={(result) => onDragEnd(result, columns, setColumns)}
+        >
+          {Object.entries(columns).map(([columnId, column], index) => {
+            return (
+              <div className="drop" key={columnId}>
+                <h2 className="drop__title">{column.name}</h2>
+                <div>
+                  <Droppable droppableId={columnId} key={columnId}>
+                    {(provided, snapshot) => {
+                      return (
+                        <div
+                          {...provided.droppableProps}
+                          ref={provided.innerRef}
+                          className="drop__space"
+                          style={{
+                            background: snapshot.isDraggingOver
+                              ? "rgba(255, 255, 255, 0.1)"
+                              : "var(--dark-color)",
+                          }}
+                        >
+                          {column.items.map((item, index) => {
+                            return (
+                              <Draggable
+                                key={item.id}
+                                draggableId={item.id}
+                                index={index}
+                              >
+                                {(provided, snapshot) => {
+                                  return (
+                                    <div
+                                      className="phrase"
+                                      ref={provided.innerRef}
+                                      {...provided.draggableProps}
+                                      {...provided.dragHandleProps}
+                                      style={{
+                                        backgroundColor: snapshot.isDragging
+                                          ? "var(--third-color)"
+                                          : "var(--dark-color)",
+                                        color: "var(--bright-color)",
+                                        ...provided.draggableProps.style,
+                                      }}
+                                    >
+                                      {item.content}
+                                    </div>
+                                  );
+                                }}
+                              </Draggable>
+                            );
+                          })}
+                          {provided.placeholder}
+                        </div>
+                      );
+                    }}
+                  </Droppable>
                 </div>
-              );
-            })}
-          </DragDropContext>
+              </div>
+            );
+          })}
+        </DragDropContext>
 
-          <form className="form-contact" onSubmit={handleSubmit}>
-            <p className="form-contact__title">Additional comments:</p>
-            <textarea
-              className="form-contact__input form-contact__input--textarea"
-              placeholder="type some additional comments"
+        <form className="form-contact" onSubmit={handleSubmit}>
+          <p className="form-contact__title">Additional comments:</p>
+          <textarea
+            className="form-contact__input form-contact__input--textarea"
+            placeholder="type some additional comments"
+            type="text"
+            name="message"
+            id="message"
+            required
+          ></textarea>{" "}
+          <div className="form-contact__input-wrapper">
+            <input
+              className="form-contact__input"
+              placeholder="name"
               type="text"
-              name="message"
-              id="message"
-              required
-            ></textarea>{" "}
-            <div className="form-contact__input-wrapper">
-              <input
-                className="form-contact__input"
-                placeholder="name"
-                type="text"
-                name="name"
-                id="name"
-              />
-              <input
-                className="form-contact__input"
-                placeholder="email"
-                type="text"
-                name="email"
-                id="email"
-              />
-            </div>
-            <div className="form-contact__buttons">
-              {/* <Button
+              name="name"
+              id="name"
+            />
+            <input
+              className="form-contact__input"
+              placeholder="email"
+              type="text"
+              name="email"
+              id="email"
+            />
+          </div>
+          <div className="form-contact__buttons">
+            {/* <Button
               className={"btn btn--dark-dark-bg"}
               onClick={handleClickClearMessage}
               text="clear"
               align="flex-start"
             /> */}
 
-              <button
-                className="btn btn--third-color"
-                style={{
-                  backgroundColor:
-                    status === "SENT"
-                      ? "var(--dark-color)"
-                      : "var(--third-color)",
-                }}
-                type="submit"
-              >
-                {status}
-              </button>
-            </div>
-          </form>
-        </div>
-        <WhyCard
-          titleOne={whyData.contact.titleOne}
-          textOne={whyData.contact.textOne}
-          titleTwo={whyData.contact.titleTwo}
-          textTwo={whyData.contact.textTwo}
-          titleThree={whyData.contact.titleThree}
-          textThree={whyData.contact.textThree}
-          titleFour={whyData.contact.titleFour}
-          textFour={whyData.contact.textFour}
-          observation={whyData.contact.observation}
-          bottom={whyData.contact.bottom}
-          left={whyData.contact.left}
-        />
-      </section>
-    </Parallax>
+            <button
+              className="btn btn--third-color"
+              style={{
+                backgroundColor:
+                  status === "SENT"
+                    ? "var(--dark-color)"
+                    : "var(--third-color)",
+              }}
+              type="submit"
+            >
+              {status}
+            </button>
+          </div>
+        </form>
+      </div>
+      <WhyCard
+        titleOne={whyData.contact.titleOne}
+        textOne={whyData.contact.textOne}
+        titleTwo={whyData.contact.titleTwo}
+        textTwo={whyData.contact.textTwo}
+        titleThree={whyData.contact.titleThree}
+        textThree={whyData.contact.textThree}
+        titleFour={whyData.contact.titleFour}
+        textFour={whyData.contact.textFour}
+        observation={whyData.contact.observation}
+        bottom={whyData.contact.bottom}
+        left={whyData.contact.left}
+      />
+    </section>
   );
 };
 
