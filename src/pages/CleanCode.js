d8e0fa8a22d727243f29aaf48f547f88f824a9f2
Kodama <fk@fkodama.com>
2022-06-09 16:14:19 -0400

    New design for clean code page

--- src/pages/CleanCode.js
+++ src/pages/CleanCode.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Parallax } from 'react-scroll-parallax';
 import '../styles/CleanCode.css';
-import WhyCard from '../components/WhyCard';
-import { whyData } from '../data/Data';
+// import WhyCard from '../components/WhyCard';
+// import { whyData } from '../data/Data';
 import ZoomIn from '../assets/images/code-check.svg';
 import ZoomOut from '../assets/images/code-check-return.svg';
 
@@ -11,25 +11,9 @@ const CleanCode = () => {
 
   const handleClick = () => {
     setShowBackground(!showBackground);
-    // setInterval(createBubble, 500);
   };
 
   useEffect(() => {}, [showBackground]);
-
-  // ------------------ BUBBLES ------------------
-
-  // const createBubble = () => {
-  //   const section = document.getElementById("code");
-  //   const createElement = document.createElement("span");
-  //   let size = Math.random() * 120;
-  //   createElement.style.width = 20 + size + "px";
-  //   createElement.style.height = 20 + size + "px";
-  //   createElement.style.left = Math.random() * window.innerWidth + "px";
-  //   section.appendChild(createElement);
-  //   setTimeout(() => {
-  //     createElement.remove();
-  //   }, 6000);
-  // };
 
   return (
     <section
@@ -39,13 +23,19 @@ const CleanCode = () => {
       }
       style={{ padding: showBackground && '35em' }}
     >
-      <Parallax opacity={[0, 1.5]}>
+      <Parallax opacity={[0, 2]}>
         <h1
           className='section-title'
           style={{ display: showBackground ? 'none' : 'block' }}
         >
-          Clean code
+          My code
         </h1>
+        <p
+          className='code__subtitle'
+          style={{ display: showBackground ? 'none' : 'block' }}
+        >
+          It must be read like a story
+        </p>
       </Parallax>
       <div className='container'>
         {/* <Parallax
@@ -53,35 +43,53 @@ const CleanCode = () => {
           opacity={[0, 1.5]}
           translateX={['-100%', '50%']}
         > */}
-        <ul
-          className='code__quote'
+
+        <Parallax opacity={[0, 2]}>
+          <div
+            className='quote'
+            style={{ display: showBackground ? 'none' : 'flex' }}
+          >
+            <q className='quote__text'>
+              “Clean code always looks like it was written by someone who
+              cares.”
+            </q>
+            <p className='quote__author-name'>
+              <span style={{ color: 'red' }}>― </span>Robert C. Martin
+            </p>
+            <p className='quote__author'>Author of Clean Code</p>
+          </div>
+        </Parallax>
+
+        {/* <ul
+          className='quote'
           style={{ display: showBackground ? 'none' : 'block' }}
         >
-          <li className='code__odd--inverse'>"clean code</li>
-          <li className='code__even--inverse'>always looks like</li>
-          <li className='code__odd--inverse'>it was written by</li>
-          <li className='code__even--inverse'>someone who</li>
-          <li className='code__odd--inverse'>cares."</li>
-          <li className='code__even--inverse code--stroke'>
-            ― Robert C. Martin
-          </li>
-          <li className='code__odd--inverse code--stroke'>
-            Author of Clean Code
-          </li>
-        </ul>
+          <li className='quote__bright quote--clean'>clean code</li>
+          <li className='quote__dark quote--always'>always</li>
+          <li className='quote__dark quote--looks'>looks like</li>
+          <li className='quote__bright quote--it-was'>it was written by</li>
+          <li className='quote__dark quote--someone'>someone</li>
+          <li className='quote__bright quote--who'>who</li>
+          <li className='quote__bright quote--cares'>cares."</li>
+          <li className='quote__bright quote--stroke'>― Robert C. Martin</li>
+          <li className='quote__bright quote--stroke'>Author of Clean Code</li>
+        </ul> */}
         {/* </Parallax> */}
         {/* <Parallax opacity={[0, 2]} translateY={['-100%', '50%']}> */}
-        <div className='code__icon-wrapper' onClick={handleClick}>
+        <div className='code__button' onClick={handleClick}>
           <img
-            className='code__icon'
             src={!showBackground ? ZoomIn : ZoomOut}
             alt='icon zoom in or out'
           />
-          <p style={{ display: !showBackground ? 'block' : 'none' }}>
+          <p
+            style={{
+              display: !showBackground ? 'block' : 'none',
+              width: '8ch',
+            }}
+          >
             CHECK MY CODE
           </p>
           <p
-            className='code--bright'
             style={{
               display: showBackground ? 'block' : 'none',
             }}
@@ -97,23 +105,25 @@ const CleanCode = () => {
           translateX={['50%', '-100%']}
         > */}
         <ul
-          className='code__words'
+          className='words'
           style={{ display: showBackground ? 'none' : 'block' }}
         >
-          <li className='code__even dry'>DRY</li>
-          <li className='code__odd meticulousness'>meticulousness</li>
-          <li className='code__even easy-read'>easy to read</li>
-          <li className='code__even easy-change'>easy to change</li>
-          <li className='code__odd naming'>naming</li>
-          <li className='code__odd conventions'>conventions</li>
-          <li className='code__even repeat'>don't repeat</li>
-          <li className='code__even self'>your self</li>
-          <li className='code__odd keep'>keep it</li>
-          <li className='code__odd simple'>simple</li>
-          <li className='code__even concise'>clear and concise</li>
-          <li className='code__odd consistency'>consistency</li>
-          <li className='code__even uncomplicate'>uncomplicate</li>
-          <li className='code__odd bem'>BEM</li>
+          <li className='words__dark words--dry'>DRY</li>
+          <li className='words__bright words--meticulousness'>
+            meticulousness
+          </li>
+          <li className='words__dark words--easy-read'>easy to read</li>
+          <li className='words__dark words--easy-change'>easy to change</li>
+          <li className='words__bright words--naming'>naming</li>
+          <li className='words__bright words--conventions'>conventions</li>
+          <li className='words__dark words--repeat'>don't repeat</li>
+          <li className='words__dark words--self'>your self</li>
+          <li className='words__bright words--keep'>keep it</li>
+          <li className='words__bright words--simple'>simple</li>
+          <li className='words__dark words--concise'>clear and concise</li>
+          <li className='words__bright words--consistency'>consistency</li>
+          <li className='words__dark words--uncomplicate'>uncomplicate</li>
+          <li className='words__bright words--bem'>BEM</li>
         </ul>
         {/* </Parallax> */}
 
