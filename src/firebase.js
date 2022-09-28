9764ac29d8c08ebbca0ffea676dd07433df74da5
Francis Kodama <fk@fkodama.com>
2022-09-28 19:30:34 -0400

    Delete unecessary comment

--- src/firebase.js
+++ src/firebase.js
@@ -1,26 +1,41 @@
 // francishkodama -------------- adapted way (with the youtube video)
 
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
+// import firebase from 'firebase/compat/app';
+// import 'firebase/compat/firestore';
 
-const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyABF0hv-AXHRMpwGFvcExzeJPad332qpr0",
-  authDomain: "fk-portfolio-third-db.firebaseapp.com",
-  projectId: "fk-portfolio-third-db",
-  storageBucket: "fk-portfolio-third-db.appspot.com",
-  messagingSenderId: "433101682998",
-  appId: "1:433101682998:web:aecbf0c40a6c6b54689b97"
-});
+// const firebaseApp = firebase.initializeApp({
+//   apiKey: "AIzaSyABF0hv-AXHRMpwGFvcExzeJPad332qpr0",
+//   authDomain: "fk-portfolio-third-db.firebaseapp.com",
+//   projectId: "fk-portfolio-third-db",
+//   storageBucket: "fk-portfolio-third-db.appspot.com",
+//   messagingSenderId: "433101682998",
+//   appId: "1:433101682998:web:aecbf0c40a6c6b54689b97"
+// 
 
-const db = firebaseApp.firestore();
+// const db = firebaseApp.firestore();
 
-export { db };
+// export { db };
 
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
 
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIRESTORE_API_KEY,
+  authDomain: process.env.REACT_APP_FIRESTORE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIRESTORE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIRESTORE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIRESTORE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIRESTORE_APP_ID,
+  measurementId: process.env.REACT_APP_FIRESTORE_MEASUREMENT_ID,
+};
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-
-
+// Populates db with all Firestore information from app
+export const db = getFirestore(app)
+//export { db };
+ 
 
 
 // francishkodama -------------- right way
